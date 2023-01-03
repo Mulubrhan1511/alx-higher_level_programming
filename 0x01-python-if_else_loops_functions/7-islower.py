5731b2a20579f23c9db1e52a31850604ab02aaff
Mulubrhan1511 <113607083+Mulubrhan1511@users.noreply.github.com>
2023-01-03 04:32:34 -0800

    mule

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+def islower(c): #return true if c is lower
+    "function that check if c is lower or upper case"
+    x = int(ord(c))
+    if x >= 97 and x <= 127:
+        return True
+    else:
+        return False
