cc9768b8c16514069e062b94831a3d5af0eaea53
Mulubrhan1511 <113607083+Mulubrhan1511@users.noreply.github.com>
2023-01-03 05:22:11 -0800

    mule

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,1 +1,9 @@
+#!/usr/bin/python3
 def print_last_digit(number):
+    if (number < 0):
+        number *= -1
+        lastdigit = number % 10
+        return lastdigit
+    else:
+        lastdigit = number % 10
+        return lastdigit
