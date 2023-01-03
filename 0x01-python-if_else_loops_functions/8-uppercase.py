f851ac252af460a693018e711a19e288d97004bf
Mulubrhan1511 <113607083+Mulubrhan1511@users.noreply.github.com>
2023-01-03 05:08:18 -0800

    mule

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,7 @@ def uppercase(str):
     for i in range(len(str)):
         if ord(str[i]) >= 97 and ord(str[i]) <= 127:
             num = 32
-            print(f"{chr(ord(str[i]) - num)}", end='')
         else:
-            print(f"{chr(ord(str[i]))}", end='')
+            num = 0
+        print(f"{chr(ord(str[i]))}", end='')
     print()
