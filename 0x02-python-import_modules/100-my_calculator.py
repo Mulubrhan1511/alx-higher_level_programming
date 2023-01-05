e5bf2639a8301c1396170b120aafa08b73fbf8a5
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2023-01-05 05:14:41 -0800

    mule

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
             a = int(sys.argv[1])
             b = int(sys.argv[3])
             if (sys.argv[2]) == '+':
-                print("{} + {} = {}".format(a, b, add(a,b)))
+                print("{} + {} = {}".format(a, b, add(a, b)))
                 exit(0)
             elif (sys.argv[2]) == '-':
                 print("{} - {} = {}".format(a, b, sub(a, b)))
