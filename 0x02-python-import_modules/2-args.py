e3b700c669bd104241f5ec8357c4425f215b5e3c
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2023-01-05 02:33:49 -0800

    mule

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    print("{} arguments:".format(len(sys.argv[1:])))
+    m = len(sys.argv[1:])
+    if m == 0:
+        print("{} arguments.".format(len(sys.argv[1:])))
+    else:
+        print("{} arguments:".format(len(sys.argv[1:])))
     j = 1
-    for i in sys.argv[1:]:
+    for i in sys.argv[1:]: 
         print("{}: {}".format(j, i))
         j += 1
