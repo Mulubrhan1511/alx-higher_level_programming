3a9a40ec266bee49983fefa66f6c78bd464419bf
Mulubrhan1511 <113607083+Mulubrhan1511@users.noreply.github.com>
2023-01-09 02:08:50 -0800

    mule

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    length = len(my_list)
-
-    if length == 0:
-        return (None)
-
-    max_int = my_list[0]
-
-    for i in range(1, length):
-        if my_list[i] > max_int:
-            max_int = my_list[i]
-
-    return (max_int)
+    max = my_list[0]
+    if len(my_list) == 0:
+        return None
+    for i in my_list:
+        if i > max:
+            max = i
+    return max
