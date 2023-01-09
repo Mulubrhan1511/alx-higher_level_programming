9d044ae029ef63b8cacf21f8436d6538dadaeb8a
Mulubrhan1511 <113607083+Mulubrhan1511@users.noreply.github.com>
2023-01-09 00:31:48 -0800

    mule

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for elem in matrix:
-        for idx, num in enumerate(elem):
-            open_string = "{}"
-            if idx != len(elem) - 1:
-                print(open_string.format(num), end=' ')
-            else:
-                print(open_string.format(num), end='')
+    for element in matrix:
+        for number in element:
+            print("{:d}".format(number), end="")
+            if(number is not element[len(element)-1]):
+                print(" ", end="")
         print()
