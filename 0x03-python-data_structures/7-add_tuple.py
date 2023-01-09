c1dd2513673c57d634634df4bb666e2d7bf492cc
Mulubrhan1511 <113607083+Mulubrhan1511@users.noreply.github.com>
2023-01-09 01:43:22 -0800

    mule

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,23 +1,10 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) < 2 and len(tuple_b) < 2:
-        if len(tuple_a) == 1 and len(tuple_b) == 1:
-            return (tuple_a[0] + tuple_b,0)
-        elif len(tuple_a) == 1 and len(tuple_b) < 1:
-            return (tuple_a,0)
-        elif len(tuple_a) < 1 and len(tuple_b) == 1:
-            return (tuple_b,0)
-        else:
-            return (0,0)
-    elif len(tuple_a) >= 2 and len(tuple_b) >= 2:
-        return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-    elif len(tuple_a) >= 2 and len(tuple_b) < 2:
-        if len(tuple_b) == 1:
-            return (tuple_a[0] + tuple_b[0],tuple_a[1])
-        else:
-            return (tuple_a[0], tuple_a[1])
-    elif len(tuple_a) < 2 and len(tuple_b) >= 2:
-        if len(tuple_a) == 1:
-            return (tuple_a[0] + tuple_b[0],tuple_b[1])
-        else:
-            return (tuple_b[0], tuple_b[1])
+
+    a = tuple_a or (0, 0)
+    b = tuple_b or (0, 0)
+    if(len(a) is 1):
+        a = (tuple_a[0], 0)
+    if(len(b) is 1):
+        b = (tuple_b[0], 0)
+    return((a[0] + b[0], a[1] + b[1]))
