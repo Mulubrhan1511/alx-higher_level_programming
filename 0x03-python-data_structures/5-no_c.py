b926bd17a55749fd05adbd61d715c1b9a46a50ee
Mulubrhan1511 <113607083+Mulubrhan1511@users.noreply.github.com>
2023-01-09 00:29:53 -0800

    mule

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def no_c(my_string):
-    new_string = ''
-    for i in my_string:
-        if i in 'cC':
-            continue
-        else:
-            new_string += i
-    return new_string
+    return(''.join(x for x in my_string if x not in 'Cc'))
