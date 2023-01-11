1be0ae295d83f051d518c0e6aeb80758197c5297
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2023-01-11 05:49:53 -0800

    mule

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,13 @@
+def weight_average(my_list=[]):
+    sum = 0
+    for i in my_list:
+        a = 1
+        for j in i:
+            a *= j
+        sum += a
+    b = 0
+    for i in my_list:
+        for j in i:
+            a = j
+        b += j
+    return sum/b
