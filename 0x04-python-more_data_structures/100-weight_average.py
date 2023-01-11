d312a2194a2093f0b85248941d71a19d263c9215
Mulubrhan1511 <113607083+Mulubrhan1511@users.noreply.github.com>
2023-01-11 05:57:07 -0800

    mule

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,13 +1,15 @@
 def weight_average(my_list=[]):
-    sum = 0
-    for i in my_list:
-        a = 1
-        for j in i:
-            a *= j
-        sum += a
-    b = 0
-    for i in my_list:
-        for j in i:
-            a = j
-        b += j
-    return sum/b
+    if(my_list):
+        sum = 0
+        for i in my_list:
+            a = 1
+            for j in i:
+                a *= j
+            sum += a
+        b = 0
+        for i in my_list:
+            for j in i:
+                a = j
+            b += j
+        return sum/b
+    return 0
