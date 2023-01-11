769b9677b26efcb30a7ee856d6a14beb8a91f39b
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2023-01-11 04:40:46 -0800

    mule

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     if (a_dictionary):
-        for i in a_dictionary:
-            a_dictionary[i] = a_dictionary[i] * 2
-    return a_dictionary
+        return(dict((x, y*2) for x, y in a_dictionary.items()))
