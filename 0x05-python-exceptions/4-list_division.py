bbd89ad71f75e73f50669b3be5bbb8e6f0cb8259
Mulubrhan1511 <113607083+Mulubrhan1511@users.noreply.github.com>
2023-01-23 03:15:41 -0800

    mule

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    new_list = []
+    for i in range(list_length):
+        try:
+            x = my_list_1[i] / my_list_2[i]
+            new_list.insert(i, x)
+        except ZeroDivisionError:
+            print("zero")
+            new_list.insert(i, 0)
+        except TypeError:
+            print("type error")
+            new_list.insert(i, 0)
+        except IndexError:
+            print("Index error")
+            new_list.insert(i, 0)
+    return new_list
