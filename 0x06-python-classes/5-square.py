2e9386f8837859528563067859afb8a6f3b3d50e
Mulubrhan1511 <113607083+Mulubrhan1511@users.noreply.github.com>
2023-01-24 03:21:02 -0800

    mule

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -45,10 +45,10 @@ class Square:
         return (self.__size ** 2)
 
     def my_print(self):
-        """
-        prints square
-        """
-        for i in range(self.__size):
-            for j in range(self.__size):
-                print("#", end='')
+        """print the square in # """
+
+        if self.__size == 0:
             print()
+
+        for i in range(self.__size):
+            print("#" * self.__size)
