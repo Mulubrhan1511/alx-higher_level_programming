bf404c0d94cd3e33a095244111cad79ba7fd4c51
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2023-01-24 02:03:27 -0800

    mule

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 1-square.py by Mulubrhan Geberkidan
+"""Defines a square """
+
+class Square:
+    """Defines a square """
+    def __init__(self, size):
+        self._Square__size = size
