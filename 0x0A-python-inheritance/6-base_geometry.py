3bcb2b42b94fac44ca9f76291dd89098e0f55168
Mulubrhan1511 <113607083+Mulubrhan1511@users.noreply.github.com>
2023-02-06 03:40:29 -0800

    mule

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""basegeometryclass"""
+
+
+class BaseGeometry:
+    """instance of geometry class"""
+
+    def __init__(self):
+        """initialize class"""
+        pass
+
+    def area(self):
+        raise Exception("area() is not implemented")
