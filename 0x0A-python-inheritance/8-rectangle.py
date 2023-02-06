d7b4fff5d44ee75ea83b22bf67ca8ca27993eba2
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2023-02-06 04:19:13 -0800

    mule

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -8,6 +8,7 @@ class BaseGeometry:
     def __init__(self):
         """initialize class"""
         pass
+
     def area(self):
         raise Exception("area() is not implemented")
 
@@ -16,8 +17,9 @@ class BaseGeometry:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
+
+
 class Rectangle(BaseGeometry):
     """Rectangle formation"""
     def __init__(self, width, height):
         self.integer_validator(width, height)
-
