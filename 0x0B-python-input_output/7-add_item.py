d58484777e928e042a34593138e03244385be29d
Mulubrhan1511 <113607083+Mulubrhan1511@users.noreply.github.com>
2023-02-07 05:51:40 -0800

    mule

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
-"""This module adds all arguments to a Python list and save them to a file."""
+"""
+this script add all arguments to a python list and
+save them into a specific given file
+"""
 
 
-import sys
+from sys import argv
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
-    try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+filename = "add_item.json"
+
+try:
+    j_list = load_from_json_file(filename)
+
+except:
+    j_list = []
+
+for arguments in argv[1:]:
+    j_list.append(arguments)
+
+save_to_json_file(j_list, filename)
