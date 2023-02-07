8994dfd2b1725d051e22ee56d351ba088e99ab30
Mulubrhan1511 <113607083+Mulubrhan1511@users.noreply.github.com>
2023-02-07 06:08:21 -0800

    mule

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-"""
-student class
-"""
+"""This module defines a class Student"""
 
 
 class Student:
-    """defines student"""
+    """Represent a student."""
+
     def __init__(self, first_name, last_name, age):
+        """Initializes a new Student
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """returns dictionary desc"""
-        return self.
+        """Gets a dictionary representation of the Student"""
+        return self.__dict__
