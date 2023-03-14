a32ba67d3b4410ce66bb459b39a93e5da1ff8423
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2023-03-14 17:00:44 +0300

    mule

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -2,11 +2,8 @@
 -- converts hbtn_0c_0 database to UTF8
 USE hbtn_0c_0;
 
-ALTER DATABASE hbtn_0c_0
-COLLATE utf8mb4_unicode_ci;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 
-ALTER TABLE first_table
-CONVERT TO CHARACTER SET utf8mb4
-COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-ALTER TABLE first_table MODIFY name VARCHAR(256) COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table CHANGE name VARCHAR(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
