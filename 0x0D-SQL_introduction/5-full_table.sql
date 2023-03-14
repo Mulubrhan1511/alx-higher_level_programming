08c9fa9d112d8e4e52012c6184315bacb6ccde9f
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2023-03-14 14:56:39 +0300

    mul

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,3 @@
 -- a script that delete the database hbtn_0c_0 in your MySQL server 
 -- because Batch 3 is the best!
-SHOW CREATE TABLE hbtn_0c_0.first_table;
+SHOW CREATE TABLE IF EXISTS hbtn_0c_0.first_table;
