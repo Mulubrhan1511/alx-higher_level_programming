d25adc6bada21e5f056e63c11b28098004270537
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2023-03-14 16:25:25 +0300

    mule

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,3 +1,3 @@
 -- a script that delete the database hbtn_0c_0 in your MySQL server 
 -- because Batch 3 is the best!
-SELECT * FROM first_table.score;
+SELECT AVG( score ) as average FROM second_table;
