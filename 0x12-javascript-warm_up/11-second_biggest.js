ab3a4384423369eb031341c1c47df99fee567cf8
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2023-04-10 16:42:45 +0300

    mule

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// Write a script that searches the second biggest integer in the list of arguments.
+if (!process.argv[2]) {
+    console.log(0);
+  } else {
+    const MyList = process.argv.slice(2);
+    const SortedList = MyList.sort();
+    console.log(SortedList[process.argv.slice(2).length - 2]);
+  } 
