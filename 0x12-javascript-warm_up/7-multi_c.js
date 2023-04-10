4b70aa92c088de28a8455791115fb04293e52ed0
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2023-04-10 15:38:56 +0300

    mule

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,7 +3,7 @@
 if (process.argv.slice(2).length < 1) {
   console.log('Missing number of occurrences');
 } else {
-    for (let i = 0; i <= parseInt(process.argv[2]); i++) {
+    for (let i = 0; i < parseInt(process.argv[2]); i++) {
         console.log('C is fun');
       }
 }
