74036960418219e6e3495099b1b6a89da52a9947
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2023-04-10 17:16:24 +0300

    mule

---
+++ 0x12-javascript-warm_up/13-add.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// Write a script that prints the addition of 2 integers
+// Function to compute the product of p1 and p2
+exports.add = function add (a, b) {
+    return a + b;
+  };
