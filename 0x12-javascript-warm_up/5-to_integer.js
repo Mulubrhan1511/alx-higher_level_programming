7069cd482f09a34d27d5348bcd8664396d4ea82b
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2023-04-10 15:09:29 +0300

    mule

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,6 +2,8 @@
 // Write a script that prints a message depending of the number of arguments passed
 if (process.argv.slice(2).length < 1) {
   console.log('Not a number');
-} else {
+} else if (parseInt(process.argv[2])) {
   console.log('My number: ' + parseInt(process.argv[2]));
+} else {
+  console.log('Not a number');
 }
