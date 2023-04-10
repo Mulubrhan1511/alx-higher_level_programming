a0676af6f90d28193da619548f3b403a5ffaf99e
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2023-04-10 16:06:18 +0300

    mule

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,13 +3,13 @@
 if (process.argv.slice(2).length < 1) {
   console.log('Missing size');
 } else if (parseInt(process.argv[2])) {
+  for (let i = 0; i < parseInt(process.argv[2]); i++) {
+    let MyVar = '';
     for (let i = 0; i < parseInt(process.argv[2]); i++) {
-        MyVar = '';
-        for (let i = 0; i < parseInt(process.argv[2]); i++) {
-            MyVar += 'X';
-        }
-        console.log(MyVar);
-      }
+      MyVar += 'X';
+    }
+    console.log(MyVar);
+  }
 } else {
-    console.log('Missing size');
+  console.log('Missing size');
 }
