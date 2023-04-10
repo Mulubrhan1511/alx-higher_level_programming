8fa269610671acb478441d78b695bc8d24d26b24
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2023-04-10 12:35:14 +0300

    mule

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,13 +1,9 @@
 #!/usr/bin/node
 // Write a script that prints a message depending of the number of arguments passed
-const arguments = process.argv.slice(2);
-if (arguments.length < 1) {
-    //  If no arguments are passed to the script, print “No argument”
-    console.log('No argument');
-  } else if (arguments.length == 1) {
-    //  If only one argument is passed to the script, print “Argument found”
-    console.log('Argument found');
-  } else {
-    //  Otherwise, print “Arguments found”
-    console.log('Arguments found');
-  }
+if (process.argv.slice(2).length < 1) {
+  console.log('No argument');
+} else if (process.argv.slice(2).length === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
