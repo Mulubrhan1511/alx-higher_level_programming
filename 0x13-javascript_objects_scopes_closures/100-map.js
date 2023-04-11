8c795652e6993c26ee70c2e122fe11439a926b8c
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2023-04-11 16:37:50 +0300

    mule

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,8 +1,5 @@
 #!/usr/bin/node
 const Mylist = require('./100-data.js').list;
-const NewList = [];
-for (i = 0; i < Mylist.length; i++) {
-    NewList.push(Mylist[i] * i);
-}
+const NewList = Mylist.map((x, y)=> x * y);
 console.log(Mylist);
 console.log(NewList);
