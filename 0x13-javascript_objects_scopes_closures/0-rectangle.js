4d24c3b873aa3ccd052c99f435ab6ac219d3a346
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2023-04-11 12:15:31 +0300

    mule

--- 0x13-javascript_objects_scopes_closures/0-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -1,6 +1,3 @@
 #!/usr/bin/node
 // Write an empty class Rectangle that defines a rectangle
-module.exports = class Rectangle {
-    constructor() {
-      }
-};
+module.exports = class Rectangle {};
