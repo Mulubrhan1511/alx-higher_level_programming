d25c6eeb0515380af992bd13f3fcaaedbdc4fccc
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2023-04-14 17:37:45 +0300

    mule

--- 0x0F-python-object_relational_mapping/0-list_databases.sql
+++ 0x0F-python-object_relational_mapping/0-list_databases.sql
@@ -1,3 +1,3 @@
 -- a script that lists all databases of your MySQL serve
 -- because Batch 3 is the best!
-SELECT * FROM cities INNER JOIN states WHERE cities.state_id = states.id;
+DROP DATABASE IF EXISTS hbtn_0e_4_usa;
--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -8,10 +8,10 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=argv[1], password=argv[2],
                          database=argv[3], charset="utf8", port=3306)
     cur = db.cursor()
-    cur.execute('SELECT cities.id, cities.name, states.name FROM cities\
-                   JOIN states\
-                   ON cities.state_id = states.id\
-                   ORDER BY cities.id ASC')
+    cur.execute('SELECT cities.id, cities.name, states.name \
+                FROM cities INNER JOIN states\
+                ON cities.state_id = states.id\
+                WHERE states.name = %s;', ('{}'.format(argv[4]),))
     rows = cur.fetchall()
     for row in rows:
         print(row)
