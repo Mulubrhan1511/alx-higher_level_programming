f73c83aee82fd8925cfffba6e49fbd4b23a62f8b
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2023-04-14 16:12:43 +0300

    mule

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     cur = db.cursor()
     cur.execute('SELECT id, name FROM states\
                    WHERE name = %s\
-                   ORDER BY states.id ASC;', ('{}'.format(argv[4]),))
+                   ORDER BY id ASC;', ('{}'.format(argv[4]),))
     rows = cur.fetchall()
     for row in rows:
         if row[1] == argv[4]:
