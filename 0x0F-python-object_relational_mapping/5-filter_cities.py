0493f46df7f78f9e93e29877b8b7f74172425ed7
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2023-04-14 18:17:49 +0300

    mule

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -8,17 +8,16 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=argv[1], password=argv[2],
                          database=argv[3], charset="utf8", port=3306)
     cur = db.cursor()
-    cur.execute('SELECT cities.id, cities.name, states.name \
+    cur.execute('SELECT cities.name \
                 FROM cities INNER JOIN states\
                 ON cities.state_id = states.id\
                 WHERE states.name = %s;', ('{}'.format(argv[4]),))
     rows = cur.fetchall()
+    list = []
     count = 0
     for row in rows:
-        count += 1
-        if count >= len(row):
-            print(row[1])
-        else:
-            print(row[1], end=', ')
+        list.append(row[0])
+    new_list = ', '.join(list)
+    print(new_list)
     cur.close()
     db.close()
