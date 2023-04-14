fe1fc20e26d116181d1590ef193cc9a3f295a9f6
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2023-04-14 15:54:11 +0300

    mule

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -8,8 +8,9 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=argv[1], password=argv[2],
                          database=argv[3], charset="utf8", port=3306)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE '{:s}' \
-                ORDER BY id ASC".format(argv[4]))
+    cur.execute('SELECT id, name FROM states\
+                   WHERE name = %s\
+                   ORDER BY states.id ASC;', ('{}'.format(argv[4]),))
     rows = cur.fetchall()
     for row in rows:
         if row[1] == argv[4]:
