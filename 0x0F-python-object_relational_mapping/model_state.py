6b736e09ea728fd0fd63e38a5a047d7e00734d91
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2023-04-15 11:30:56 +0300

    mule

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -6,17 +6,15 @@ Desc: This module contains a python script that contains the
 class definition of a State and an instance Base = declarative_base()
 """
 
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
-
 Base = declarative_base()
-
 
 class State(Base):
     """
     This class Inherits from Base and links to the MySQL table states.
     """
-    __tablename__ = "states"
+    __tablename__ = 'states'
     id = Column(Integer, primary_key=True)
-    name = Column(String(128), nullable=False)
+    name =  Column(String(128), nullable=False)
