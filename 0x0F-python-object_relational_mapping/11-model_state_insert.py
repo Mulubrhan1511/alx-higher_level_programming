c6abb5f77e2e0648c030f2c0ff18fa3bdddeb254
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2023-04-16 11:40:02 +0300

    mule

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 """
-Write a script that adds the State object “Louisiana” to the database hbtn_0e_6_usa
+Write a script that adds the State
+object “Louisiana” to the database hbtn_0e_6_usa
 """
 
 from model_state import Base, State
