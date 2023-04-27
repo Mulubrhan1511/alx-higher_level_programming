fd5d4767c22e14d43f0ecdb83aa86649f21a50a3
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2023-04-27 22:10:35 +0300

    mule

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -11,21 +11,21 @@ def find_peak(list_of_integers):
     """
     size = len(list_of_integers)
     mid_e = size
-    mida = size // 2
+    mid = size // 2
 
     if size == 0:
         return None
 
     while True:
         mid_e = mid_e // 2
-        if (mida < size - 1 and
-                list_of_integers[mida] < list_of_integers[mida + 1]):
+        if (mid < size - 1 and
+                list_of_integers[mid] < list_of_integers[mid + 1]):
             if mid_e // 2 == 0:
                 mid_e = 2
-            mida = mida + mid_e // 2
-        elif mid_e > 0 and list_of_integers[mida] < list_of_integers[mida - 1]:
+            mid = mid + mid_e // 2
+        elif mid_e > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
             if mid_e // 2 == 0:
                 mid_e = 2
-            mida = mida - mid_e // 2
+            mid = mid - mid_e // 2
         else:
-            return list_of_integers[mida]
+            return list_of_integers[mid]
