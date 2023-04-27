df761d40eab324d015ba0f037b4ffe21ba2e72b9
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2023-04-27 21:59:58 +0300

    mule

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,6 +2,7 @@
 """ Test function find_peak """
 
 def find_peak(list_of_integers):
+    """ Test function find_peak """
     if len(list_of_integers) == 0:
         return None
     size = len(list_of_integers)
