f5bee096ff9df4a7d474f3cd5bf030b7eac98a1e
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2023-04-28 22:42:47 +0300

    mule

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Write a Python script that fetches https://alx-intranet.hbtn.io/status """
+import requests
+import sys
+
+if __name__ == "__main__":
+    repos = sys.argv[2]
+    commit = sys.argv[1]
+    url = 'https://api.github.com/repos/' + repos + '/' + commit + '/commits'
+    r = requests.get(url)
+    my_fiile = r.json()
+    for i in range(10):
+        print('{}: {}'.format(commit.get('sha'),commit.get('commit').get('author').get('name')))
