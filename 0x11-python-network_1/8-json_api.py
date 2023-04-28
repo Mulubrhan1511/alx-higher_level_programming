1782e991eb250a1c98b28efb77699b9f00d5f1e2
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2023-04-28 22:00:29 +0300

    mule

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     if len(sys.argv) <= 1:
         payload = {"q": ""}
     else:
-        payload = {"q": sys.argv[0]}
+        payload = {"q": sys.argv[1]}
     r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
     try:
         file = r.json()
