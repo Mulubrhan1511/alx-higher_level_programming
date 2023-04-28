6fc773f88aca7ffdf05f1b842595ecc5ad80a541
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2023-04-28 22:23:53 +0300

    mule

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-""" Write a Python script that fetches https://alx-intranet.hbtn.io/status """
-import requests
+"""Uses the GitHub API to display a GitHub ID based on given credentials.
+"""
 import sys
+import requests
+from requests.auth import HTTPBasicAuth
+
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    file = requests.auth.HTTPBasicAuth(username, password)
-    r = requests.get("https://api.github.com/user", auth=file)
-    print(r.json().get("id"))
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    req = requests.get("https://api.github.com/user", auth=auth)
+    print(req.json().get("id"))
