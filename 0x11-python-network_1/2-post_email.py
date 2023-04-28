069e0efd44f46559fdb0e335bfb45dce0ec61e08
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2023-04-28 16:25:14 +0300

    mule

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -6,8 +6,9 @@ import sys
 
 
 if __name__ == "__main__":
-
-    my_file = urllib.parse.urlencode({'email': sys.argv[2]}).encode('utf-8')
-    request = urllib.request.Request(sys.argv[1], my_file)
-    with urllib.request.urlopen(request) as response:
+    values = {'email' : sys.argv[2]}
+    data = urllib.parse.urlencode(values)
+    data = data.encode('utf-8')
+    req = urllib.request.Request(sys.argv[1], data)
+    with urllib.request.urlopen(req) as response:
         print(response.read().decode('utf-8'))
