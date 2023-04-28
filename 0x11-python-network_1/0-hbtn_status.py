367a99b202b82ff4dc0f7c047da0a4225986d630
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2023-04-28 13:20:40 +0300

    mule

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,10 +3,10 @@
 
 if __name__ == "__main__":
     import urllib.request
-
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    web = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(web) as response:
         my_file = response.read()
-    print("Body response:")    
+    print("Body response:")
     print("\t- type: {}".format(type(my_file)))
     print("\t- content: {}".format(my_file))
     print("\t- utf8 content: {}".format(my_file.decode('utf-8')))
