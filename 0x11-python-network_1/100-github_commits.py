7a2312b071614a6429dd38b454f8289a624fdca8
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2023-04-28 22:49:48 +0300

    mule

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -6,10 +6,10 @@ import sys
 if __name__ == "__main__":
     repos = sys.argv[2]
     commit = sys.argv[1]
-    url = 'https://api.github.com/repos/' + repos + '/' + commit + '/commits'
+    url = "https://api.github.com/repos/{}/{}/commits".format(sys.argv[2], sys.argv[1])
     r = requests.get(url)
     my_fiile = r.json()
     for i in range(10):
             print("{}: {}".format(
                 my_fiile[i].get("sha"),
-                commits[i].get("commit").get("author").get("name")))
+                my_fiile[i].get("commit").get("author").get("name")))
