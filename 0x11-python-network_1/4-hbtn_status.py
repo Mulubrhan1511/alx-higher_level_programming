8fdf7eb575c9b3e596ac7cc3c5ff1ef3d691c6f0
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2023-04-28 17:00:03 +0300

    mule

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """ takes URL, email and  encodes the email """
 
-import request
+from requests import get
 
 if __name__ == "__main__":
-    req = requests.get("https://alx-intranet.hbtn.io/status")
+    req = get("https://alx-intranet.hbtn.io/status")
     print("Body response:")
     print("\t- type: {}".format(type(req.text)))
     print("\t- content: {}".format(req.text))
