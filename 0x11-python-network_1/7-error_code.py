3bd2ec23ffca2e2cd2157d95672887fb88c2414a
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2023-04-28 19:34:21 +0300

    mule

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -5,8 +5,7 @@ import sys
 
 if __name__ == "__main__":
     r = requests.get(sys.argv[1])
-    m = r.status_code
-    if m >= 400:
-        print("Error code: {}".format(m))
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
     else:
         print(r.text)
