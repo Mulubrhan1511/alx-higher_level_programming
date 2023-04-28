a18843d9e1f22ed81db147c8b47c1387586be5e2
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2023-04-28 22:17:23 +0300

    mule

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Write a Python script that fetches https://alx-intranet.hbtn.io/status """
+import requests
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[0]
+    password = sys.argv[1]
+    file = requests.auth.HTTPBasicAuth(username, password)
+    r = requests.get("https://api.github.com/user", auth=file)
+    print(r.json().get("id"))
