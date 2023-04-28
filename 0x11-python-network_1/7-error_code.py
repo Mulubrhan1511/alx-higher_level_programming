655506fb38d8922b3e9053ac242578bcc5e5c536
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2023-04-28 19:30:55 +0300

    mule

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Write a Python script that fetches https://alx-intranet.hbtn.io/status """
+import requests
+import sys
+
+if __name__ == "__main__":
+    r = requests.get(sys.argv[1])
+    m = r.status_code
+    if m >= 400:
+        print('Error code: ' + m)
+    else:
+        print(r.text)
