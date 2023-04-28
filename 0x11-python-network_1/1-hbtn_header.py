04fac238caae7c0e5e44a901c3fea0545c17e2da
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2023-04-28 13:42:15 +0300

    mule

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -5,8 +5,8 @@ from sys import argv
 import sys
 
 if __name__ == "__main__":
-    url = sys.argv[1]
+    my_url = sys.argv[1]
 
-    with urllib.request.urlopen(url) as response:
+    with urllib.request.urlopen(my_url) as response:
         headers = response.info()
         print(headers.get('X-Request-Id'))
