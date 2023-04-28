618b7a3f616a274b40eb7c96cfd45b0a640215c0
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2023-04-28 19:07:05 +0300

    mule

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Write a Python script that fetches https://alx-intranet.hbtn.io/status """
-import requests 
-import sys 
+import requests
+import sys
 
 if __name__ == "__main__":
     pay = {'email': sys.argv[2]}
