3b60e68a3d1c25fb58701dc05642dd64986dda7a
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2023-04-28 17:13:51 +0300

    mule

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 """ Write a Python script that fetches https://alx-intranet.hbtn.io/status """
-import urllib.request
+import requests
 import sys
 
 if __name__ == "__main__":
     my_url = sys.argv[1]
-
-    with urllib.request.urlopen(my_url) as response:
-        headers = response.info()
-        print(headers.get('X-Request-Id'))
+    r = requests.get(sys.argv[1])
+    print(r.headers.get('X-Request-Id'))
