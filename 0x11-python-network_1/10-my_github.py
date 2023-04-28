976538426771c3c07dd022d9157af8d5195b2c73
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2023-04-28 22:20:44 +0300

    mule

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -4,8 +4,8 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    username = sys.argv[0]
-    password = sys.argv[1]
+    username = sys.argv[1]
+    password = sys.argv[2]
     file = requests.auth.HTTPBasicAuth(username, password)
     r = requests.get("https://api.github.com/user", auth=file)
     print(r.json().get("id"))
