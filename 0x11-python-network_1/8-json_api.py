e8c739dcf064a478faa04d7df23c427f50772c81
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2023-04-28 21:06:10 +0300

    mule

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Write a Python script that fetches https://alx-intranet.hbtn.io/status """
+import requests
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) <= 1:
+        payload = {'q': ''}
+    else:
+        payload = {'q': sys.argv[0]}
+    try:
+        r = requests.get('http://0.0.0.0:5000/search_user')
+        file = r.json()
+        if file == {}:
+            print('No result')
+        else:
+            print("[{}] {}".format(file.get("id"), file.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
